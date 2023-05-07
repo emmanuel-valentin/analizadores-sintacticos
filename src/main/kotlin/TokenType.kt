ddefa1ab3270e58141ad9a1391941efa4276df4d
Emmanuel Valentin <ema_110@outlook.es>
2023-05-07 14:02:14 -0600

    Add TokenType enum

---
+++ src/main/kotlin/TokenType.kt
@@ -0,0 +1,17 @@
+enum class TokenType {
+  // Identificadores
+  IDENTIFIER,
+
+  // Palabaras reservadas
+  SELECT,
+  FROM,
+  DISTINCT,
+
+  // Carácteres
+  COMMA,
+  DOT,
+  ASTERISK,
+
+  // Final de cadena
+  EOF,
+}
