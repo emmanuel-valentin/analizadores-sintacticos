a9856b312b7423ad87a5f48890682ec83e9e9018
alegjs044 <alegjs044@gmail.com>
2023-06-04 14:14:55 -0600

    Update gammar of Parser.kt

--- src/main/kotlin/Parser.kt
+++ src/main/kotlin/Parser.kt
@@ -11,3 +11,53 @@ class Parser(private val tokens: List<Token>) {
   private val identifier = Token(TokenType.IDENTIFIER, "")
   private val eof = Token(TokenType.EOF, "")
   private val epsilon = Token(TokenType.EPSILON, "")
+
+  private val parsingTable: Map<String, Map<Token, MutableList<*>>>
+
+  init {
+    parsingTable = mapOf(
+      "Q" to mapOf(
+        select to mutableListOf(select, "D", from, "T")
+      ),
+      "D" to mapOf(
+        distinct to mutableListOf(distinct, "P"),
+        asterisk to mutableListOf("P"),
+        identifier to mutableListOf("P")
+      ),
+      "P" to mapOf(
+        asterisk to mutableListOf(asterisk),
+        identifier to mutableListOf("A")
+      ),
+      "A" to mapOf(
+        identifier to mutableListOf("A2", "A1")
+      ),
+      "A1" to mapOf(
+        comma to mutableListOf(comma, "A"),
+        from to mutableListOf(epsilon)
+      ),
+      "A2" to mapOf(
+        identifier to mutableListOf(identifier, "A3")
+      ),
+      "A3" to mapOf(
+        from to mutableListOf(epsilon),
+        comma to mutableListOf(epsilon),
+        dot to mutableListOf(dot, identifier),
+      ),
+      "T" to mapOf(
+        identifier to mutableListOf("T2", "T1")
+      ),
+      "T1" to mapOf(
+        comma to mutableListOf(comma, "T"),
+        eof to mutableListOf(epsilon)
+      ),
+      "T2" to mapOf(
+        identifier to mutableListOf(identifier, "T3")
+      ),
+      "T3" to mapOf(
+        comma to mutableListOf(epsilon),
+        identifier to mutableListOf(identifier),
+        eof to mutableListOf(epsilon)
+      )
+    )
+  }
+
