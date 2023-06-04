f92b194c24fe84682c5fb4d1d32c5bcd9c278440
alegjs044 <alegjs044@gmail.com>
2023-06-04 14:04:40 -0600

    Update Parser.kt

--- src/main/kotlin/Parser.kt
+++ src/main/kotlin/Parser.kt
@@ -60,3 +60,99 @@ class Parser(private val tokens: List<Token>) {
     16 to mapOf(T3 to listOf(id)),
     17 to mapOf(T3 to listOf())
   )
+
+  // Tablas de análisis sintáctico
+  private val actionTable = mapOf(
+    0 to mapOf(select to Pair(shift, 2)),
+    1 to mapOf(eof to Pair(accept, null)),
+    2 to mapOf(
+      distinct to Pair(shift, 4),
+      asterisk to Pair(shift, 6),
+      id to Pair(shift, 9)
+    ),
+    3 to mapOf(from to Pair(shift, 10)),
+    4 to mapOf(
+      asterisk to Pair(shift, 6),
+      id to Pair(shift, 9),
+    ),
+    5 to mapOf(from to Pair(reduce, 3)),
+    6 to mapOf(from to Pair(reduce, 4)),
+    7 to mapOf(from to Pair(reduce, 5)),
+    8 to mapOf(
+      from to Pair(reduce, 8),
+      comma to Pair(shift, 13)
+    ),
+    9 to mapOf(
+      from to Pair(reduce, 11),
+      comma to Pair(reduce, 11),
+      dot to Pair(shift, 15)
+    ),
+    10 to mapOf(id to Pair(shift, 22)),
+    11 to mapOf(from to Pair(reduce, 2)),
+    12 to mapOf(from to Pair(reduce, 6)),
+    13 to mapOf(id to Pair(shift, 9)),
+    14 to mapOf(
+      from to Pair(reduce, 9),
+      comma to Pair(reduce, 9)
+    ),
+    15 to mapOf(id to Pair(shift, 17)),
+    16 to mapOf(eof to Pair(reduce, 1)),
+    17 to mapOf(
+      from to Pair(reduce, 10),
+      comma to Pair(reduce, 10),
+    ),
+    18 to mapOf(
+      comma to Pair(shift, 20),
+      eof to Pair(reduce, 14)
+    ),
+    19 to mapOf(eof to Pair(reduce, 12)),
+    20 to mapOf(id to Pair(shift, 22)),
+    21 to mapOf(from to Pair(reduce, 7)),
+    22 to mapOf(
+      comma to Pair(reduce, 17),
+      id to Pair(shift, 24),
+      eof to Pair(reduce, 17)
+    ),
+    23 to mapOf(
+      comma to Pair(reduce, 15),
+      eof to Pair(reduce, 15)
+    ),
+    24 to mapOf(
+      comma to Pair(reduce, 16),
+      eof to Pair(reduce, 16)
+    ),
+    25 to mapOf(
+      eof to Pair(reduce, 13)
+    )
+  )
+
+  private val gotoTable = mapOf(
+    0 to mapOf(Q to 1),
+    2 to mapOf(
+      D to 3,
+      P to 5,
+      A to 7,
+      A2 to 8,
+    ),
+    4 to mapOf(
+      P to 11,
+      A to 7,
+      A2 to 8,
+    ),
+    8 to mapOf(A1 to 12),
+    9 to mapOf(A3 to 14),
+    10 to mapOf(
+      T to 16,
+      T2 to 18,
+    ),
+    13 to mapOf(
+      A to 21,
+      A2 to 8,
+    ),
+    18 to mapOf(T1 to 19),
+    20 to mapOf(
+      T to 25,
+      T2 to 18,
+    ),
+    22 to mapOf(T3 to 23),
+  )
