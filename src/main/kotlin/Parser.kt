8f6fcc44431821ecaba3cb40561e2aa6832e64cd
Emmanuel Valentin <ema_110@outlook.es>
2023-06-05 00:19:17 -0600

    Parser.kt (Analizador finalizado): Add token epsilon and refactor parse function

--- src/main/kotlin/Parser.kt
+++ src/main/kotlin/Parser.kt
@@ -12,71 +12,67 @@ class Parser(private val tokens: List<Token>) {
   private val eof = Token(TokenType.EOF, "")
   private val epsilon = Token(TokenType.EPSILON, "")
 
-  private val parsingTable: Map<String, Map<Token, MutableList<*>>>
-
-  init {
-    parsingTable = mapOf(
-      "Q" to mapOf(
-        select to mutableListOf(select, "D", from, "T")
-      ),
-      "D" to mapOf(
-        distinct to mutableListOf(distinct, "P"),
-        asterisk to mutableListOf("P"),
-        identifier to mutableListOf("P")
-      ),
-      "P" to mapOf(
-        asterisk to mutableListOf(asterisk),
-        identifier to mutableListOf("A")
-      ),
-      "A" to mapOf(
-        identifier to mutableListOf("A2", "A1")
-      ),
-      "A1" to mapOf(
-        comma to mutableListOf(comma, "A"),
-        from to mutableListOf(epsilon)
-      ),
-      "A2" to mapOf(
-        identifier to mutableListOf(identifier, "A3")
-      ),
-      "A3" to mapOf(
-        from to mutableListOf(epsilon),
-        comma to mutableListOf(epsilon),
-        dot to mutableListOf(dot, identifier),
-      ),
-      "T" to mapOf(
-        identifier to mutableListOf("T2", "T1")
-      ),
-      "T1" to mapOf(
-        comma to mutableListOf(comma, "T"),
-        eof to mutableListOf(epsilon)
-      ),
-      "T2" to mapOf(
-        identifier to mutableListOf(identifier, "T3")
-      ),
-      "T3" to mapOf(
-        comma to mutableListOf(epsilon),
-        identifier to mutableListOf(identifier),
-        eof to mutableListOf(epsilon)
-      )
+  private val parsingTable = mapOf(
+    "Q" to mapOf(
+      select to mutableListOf(select, "D", from, "T")
+    ),
+    "D" to mapOf(
+      distinct to mutableListOf(distinct, "P"),
+      asterisk to mutableListOf("P"),
+      identifier to mutableListOf("P")
+    ),
+    "P" to mapOf(
+      asterisk to mutableListOf(asterisk),
+      identifier to mutableListOf("A")
+    ),
+    "A" to mapOf(
+      identifier to mutableListOf("A2", "A1")
+    ),
+    "A1" to mapOf(
+      comma to mutableListOf(comma, "A"),
+      from to mutableListOf(epsilon)
+    ),
+    "A2" to mapOf(
+      identifier to mutableListOf(identifier, "A3")
+    ),
+    "A3" to mapOf(
+      from to mutableListOf(epsilon),
+      comma to mutableListOf(epsilon),
+      dot to mutableListOf(dot, identifier),
+    ),
+    "T" to mapOf(
+      identifier to mutableListOf("T2", "T1")
+    ),
+    "T1" to mapOf(
+      comma to mutableListOf(comma, "T"),
+      eof to mutableListOf(epsilon)
+    ),
+    "T2" to mapOf(
+      identifier to mutableListOf(identifier, "T3")
+    ),
+    "T3" to mapOf(
+      comma to mutableListOf(epsilon),
+      identifier to mutableListOf(identifier),
+      eof to mutableListOf(epsilon)
     )
-  }
+  )
+
+  private val stack = Stack<Any>()
 
 
  fun parse() {
     var i = 0
     var lookahead = tokens[i]
-    val symbolStack = Stack<Any>()
-    symbolStack.push(eof)
-    symbolStack.push("Q")
+    stack.push(eof)
+    stack.push("Q")
 
-    var topValueStack = symbolStack.peek()
+    var topValueStack = stack.peek()
 
-    while (symbolStack.isNotEmpty()) {
+    while (stack.isNotEmpty() && i < (tokens.size - 1)) {
 
       if (lookahead == topValueStack) {
-        symbolStack.pop()
-        if (++i >= tokens.size) break
-        lookahead = tokens[i]
+        stack.pop()
+        lookahead = tokens[++i]
       }
       else if (topValueStack is Token) {
         error(lookahead.position, "Consulta no válida")
@@ -88,19 +84,19 @@ class Parser(private val tokens: List<Token>) {
       }
       else {
         val productions = getProductions(topValueStack, lookahead)?.toMutableList()
-        symbolStack.pop()
+        stack.pop()
         while (!productions.isNullOrEmpty()) {
           val lastProduction = productions.last()
 
           if (lastProduction != epsilon) {
-            symbolStack.push(lastProduction)
+            stack.push(lastProduction)
           }
 
           productions.remove(lastProduction)
         }
       }
 
-      topValueStack = symbolStack.peek()
+      topValueStack = stack.peek()
     }
     println("Consulta válida")
   }
--- src/main/kotlin/TokenType.kt
+++ src/main/kotlin/TokenType.kt
@@ -14,4 +14,5 @@ enum class TokenType {
 
   // Final de cadena
   EOF,
+  EPSILON,
 }
